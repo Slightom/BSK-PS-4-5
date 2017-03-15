651788b7bd138545abcf7915142aaaea262e498f
Tomasz Suchwalko <tomasz.suchwalko@gmail.com>
2017-03-15 21:41:34 +0100

    finished 1 and 2. Not committed by doctor

--- BSK-PS-4-5/BSK-PS-4-5/Program.cs
+++ BSK-PS-4-5/BSK-PS-4-5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,14 +18,29 @@ namespace BSK_PS_4_5
         public GeneratorLFSR(String f, String s)
         {
             indexes = new List<int>();
+            init(f, s);           
+        }
+
+        public void init(String f, String s)
+        {
+            indexes.Clear();
+
             this.l = s.Length;
             this.tmp = new int[l];
             this.seed = new int[l]; // to be frank it is unnecessary. We need tmp which is updated after every generating bit
 
             for (int i = 0; i < l; i++)
             {
-                if (f[i].Equals('1'))   { indexes.Add(i); } // determine which digits will be taking to XOR operation
+                if (f[i].Equals('1')) { indexes.Add(i); } // determine which digits will be taking to XOR operation
                 seed[i] = tmp[i] = (int)Char.GetNumericValue(s[i]); // ascii 49 -> 1
+            }
+        }
+
+        public void reset() // we recover to state at begining
+        {
+            for (int i=0; i<l; i++)
+            {
+                tmp[i] = seed[i]; 
             }
         }
 
@@ -60,10 +76,112 @@ namespace BSK_PS_4_5
     {
         static void Main(string[] args)
         {
-            int l, i;
-            string s, f; // seed, polynomial function
+            int i;
+            string s="", f=""; // seed, polynomial function
+            string fileName = "test3";
             GeneratorLFSR generatorLFSR;
+            byte[] fileBytes;
+            byte[] encodedBytes;
+            byte[] decodedBytes;
+            BinaryWriter br;
+            bool b = true;
 
+
+            setFunctionAndSeed(ref f, ref s);
+
+            generatorLFSR = new GeneratorLFSR(f, s);
+
+            // testing, generate four bits 
+            //Console.Write("{0,26}", "Y: ");
+            //Console.WriteLine("{0}{1}{2}{3}", generatorLFSR.getOneBit(), generatorLFSR.getOneBit(), generatorLFSR.getOneBit(), generatorLFSR.getOneBit());
+
+
+            while (b)
+            {
+                Console.WriteLine("*****************************************************************************************\n"
+                                 + "f:    " + f + "\n"
+                                 + "seed: " + s + "\n"
+                                 + "file: " + fileName + "\n"
+                                 + "Choose action: change function and seed(c) | change file(cf) | 2e | 2d | 3e | 3d | q");
+                switch (Console.ReadLine())
+                {
+                    case "c":
+                        setFunctionAndSeed(ref f, ref s);
+                        generatorLFSR.init(f, s);
+                        break;
+
+                    case "cf":
+                        setFile(ref fileName);
+                        break;
+
+                    case "2e":
+                        // reading all bytes from file
+                        fileBytes = File.ReadAllBytes(System.IO.Directory.GetCurrentDirectory() + @"\" + fileName + ".bin");
+                        // encoding
+                        encodedBytes = exercise2encode(fileBytes, generatorLFSR);
+                        // write all bytes into the file
+                        br = new BinaryWriter(new FileStream(System.IO.Directory.GetCurrentDirectory() + @"\" + fileName + "encoded.bin", FileMode.Create));
+                        br.Write(encodedBytes);
+                        br.Close();
+                        break;
+                    case "2d":
+                        // reading all bytes from file
+                        fileBytes = File.ReadAllBytes(System.IO.Directory.GetCurrentDirectory() + @"\" + fileName + "encoded.bin");
+                        // decoding
+                        decodedBytes = exercise2decode(fileBytes, generatorLFSR);
+                        // write all bytes into the file
+                        br = new BinaryWriter(new FileStream(System.IO.Directory.GetCurrentDirectory() + @"\" + fileName + ".bin", FileMode.Create));
+                        br.Write(decodedBytes);
+                        br.Close();
+                        break;
+                    case "3e":
+                      
+                        break;
+                    case "3d":
+
+                        break;
+
+                    case "q":
+                        b = false;
+                        break;
+
+                }
+            }
+
+
+            Console.ReadKey();
+
+        }
+
+        private static byte[] exercise2decode(byte[] fileBytes, GeneratorLFSR generatorLFSR)
+        {
+            generatorLFSR.reset();
+
+            byte[] decodedBytes = new byte[fileBytes.Length];
+            StringBuilder s = new StringBuilder();
+
+            for (int i = 0; i < fileBytes.Length; i++)
+            {
+                s.Clear();
+                s.Insert(0, Convert.ToString(fileBytes[i], 2).PadLeft(8, '0')); // we have string of 8 digits representing one byte. Example 00001100
+                s = updateS(s, generatorLFSR);
+
+                decodedBytes[i] = Convert.ToByte(s.ToString(), 2);
+            }
+
+            return decodedBytes;
+
+        }
+
+        private static void setFile(ref string fileName)
+        {
+            Console.WriteLine("Podaj nazwę pliku(bez rozszerzenia): ");
+            fileName = Console.ReadLine();
+        }
+
+        private static void setFunctionAndSeed(ref string f, ref string s)
+        {
+            int l;
             Console.Write("{0,26}", "Podaj funkcję wielomianu: ");
             f = Console.ReadLine();
             l = f.Length;
@@ -77,15 +195,35 @@ namespace BSK_PS_4_5
                                  + "{0,26}", "Podaj seed: ");
                 s = Console.ReadLine();
             }
-
-            generatorLFSR = new GeneratorLFSR(f, s);
-
-            // testing, generate four bits 
-            Console.Write("{0,26}", "Y: ");
-            Console.WriteLine("{0}{1}{2}{3}", generatorLFSR.getOneBit(), generatorLFSR.getOneBit(), generatorLFSR.getOneBit(), generatorLFSR.getOneBit());
-
         }
 
+
+        private static byte[] exercise2encode(byte[] fileBytes, GeneratorLFSR generatorLFSR)
+        {
+            byte[] encodedBytes = new byte[fileBytes.Length];
+            StringBuilder s = new StringBuilder();
+
+            for(int i=0; i<fileBytes.Length; i++)
+            {
+                s.Clear();
+                s.Insert(0, Convert.ToString(fileBytes[i], 2).PadLeft(8, '0')); // we have string of 8 digits representing one byte. Example 00001100
+                s = updateS(s, generatorLFSR);
+
+                encodedBytes[i] = Convert.ToByte(s.ToString(),2);
+            }
+
+            return encodedBytes;
+        }
+
+        private static StringBuilder updateS(StringBuilder s, GeneratorLFSR generatorLFSR)
+        {
+            for(int i=0; i<s.Length; i++) // makes XOR for all digits
+            {
+                s[i] = (char)((int)s[i] ^ generatorLFSR.getOneBit()); 
+            }
+
+            return s;
+        }
 
     }
 }
